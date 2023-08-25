dff5733617c7e0b9034391936e72d4f96f4d31da
Arno Klein <binarybottle@gmail.com>
2023-08-25 16:37:49 -0400

    Add test

--- call_gpt.py
+++ call_gpt.py
@@ -27,3 +27,15 @@ def generate_chatgpt_response(prompt, model=gpt_model):
 
     return response
 
+
+#output = openai.ChatCompletion.create(
+#  model="gpt-4",
+#  messages=[
+#     {"role": "system", "content": "You are a helpful assistant."},
+#        {"role": "user", "content": "Who won the world series in 2020?"},
+#        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+#        {"role": "user", "content": "Where was it played?"}
+#    ]
+#)
+#print(output)
+
