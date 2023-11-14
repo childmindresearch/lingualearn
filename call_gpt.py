e015f3692b13714c262679741992396960d70228
Arno Klein <binarybottle@gmail.com>
2023-11-14 12:25:52 -0500

    Add jupyter notebook to transcribe and summarize audio files.

--- call_gpt.py
+++ call_gpt.py
@@ -16,7 +16,7 @@ from io_files import json2png
 #-----------------------------------------------------------------------------                                              
 gpt_model = "gpt-4"  #"gpt-3.5-turbo" #"text-davinci-003"
 if gpt_model == "gpt-4":
-    openai.api_key = os.getenv("OPENAI4_API_KEY")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 elif gpt_model == "gpt-3.5-turbo":  #"text-davinci-003"
     openai.api_key = os.getenv("OPENAI3_API_KEY")
 
